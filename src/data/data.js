21422aaadd6e61a995e6bdb391fafac1791bb210
Dhanush Rajesh <dhanushr0405@gmail.com>
2024-09-03 18:02:35 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,33 @@
+import WorkIm from "../assets/aws.png";
+import RealEst from "../assets/javascript.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: WorkIm,
+    github: "",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "React JS Application",
+    image: RealEst,
+    github: "",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: WorkIm,
+    github: "",
+    live: "",
+  },
+  {
+    id: 4,
+    name: "React JS Application",
+    image: RealEst,
+    github: "",
+    live: "",
+  },
+];
